9e3fd50a219b6899de8040a34c7a784bbe9da566
Sakib <56402127+sakibovi123@users.noreply.github.com>
2021-01-21 14:43:26 +0600

    Add files via upload

---
+++ lib/config.dart
@@ -0,0 +1,11 @@
+class Config{
+  static String key = "ck_6634951ca5a38c75a394acca10e3cb8bf2d7ca80";
+  static String secret = "cs_c4ba97c49285eb8cd3bbcb4b7fbcfe41f7dc9feb";
+  static String  url = "https://gngbd.xyz/wp-json/wc/v3";
+  static String customerURL = "/customers";
+  static String tokenUR = "https://gngbd.xyz/wp-json/jwt-auth/v1/token";
+  static String categoryURL = "/products/categories";
+  static String productsURL = "/products";
+  static String todayOffersTagId = "20";
+  
+}
